298c68eb77ce5bfe0e6fc13fc23ecd830109a658
unknown <manyamsanjay27@gmail.com>
2026-09-14 10:37:58 +0530

    added Score

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 function getRnd(){
     return Math.floor(Math.random()* 3) + 1
 }
+let score = 0;
 function getChoice(ch){
     let cpu = getRnd();
-    const contdiv = document.querySelector(".content");
+    const contdiv = document.querySelector("#res");
+    const scorediv = document.querySelector("#score");
     const emojis = {
         1:"🪨",
         2:"📃",
         3:"✂️"
     };
+    const emojcpu = emojis[cpu];
     if (cpu == ch){
-        contdiv.innerHTML = "Draw 🥀";
+        contdiv.innerHTML = `Draw 🥀 (CPU = ${emojcpu})`;
     }
     else if ((ch == 1 && cpu == 3)||(ch == 2 && cpu == 1)||(ch == 3 && cpu == 2)){
-        contdiv.innerHTML = "Win 🥳!!"
+        contdiv.innerHTML = `Win 🥳!! (CPU = ${emojcpu})`;
+        score = score + 1;
+        scorediv.innerHTML = score;
     }
     else{
-        contdiv.innerHTML = "Lose 🥺"
+        contdiv.innerHTML = `Lose 🥺 (CPU = ${emojcpu})`;
+        score = score - 1;
+        scorediv.innerHTML = score;
     }
 }
